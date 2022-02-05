ec4b343a0ee9874c7fed765db7ded625320184a5
Umar <umarkhatab467@gmail.com>
2022-02-05 06:05:51 +0500

    made a person template for all products

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 document.write("Hello World");
+
+//  Make Secen Nodes
+
+// Medicines/Shoes/Perfumes are products
+
+class product {
+  constructor(name, price) {
+    this.name = name;
+    this.price = price;
+    this.date = Number(new Date());
+  }
+}
+
+const perfume = new product("Blue", 23);
